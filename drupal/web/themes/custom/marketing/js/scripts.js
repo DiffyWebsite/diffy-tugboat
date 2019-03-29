17889922b6f931f4e0247a8d2ae41149032dbe1c
vlikin <viktor.likin@gmail.com>
2019-03-29 15:15:31 +0200

    Theme assets.

---
+++ drupal/web/themes/custom/marketing/js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+  $('.navbar-toggler').click(function() {
+    $('.navbar-block__mobile--overlay').toggleClass('js-navbar-block__mobile--show');
+    $('.site-logo').toggleClass('js-logo-color');
+    $('body').toggleClass('js-mobile-menu');
+    $('.js-button--change').toggleClass('fa-bars fa-times');
+  });
+
+  $('.js-hidden').removeClass('js-hidden');
+
+  $('.pricing-card__head').matchHeight({
+    property: 'height'
+  });
+  $('.pricing-card__body').matchHeight({
+    property: 'height'
+  });
+  $('.pricing-card__bottom').matchHeight({
+    property: 'height'
+  });
+});
