356849dab448936bb373caa9735faca4d8beb7d7
Sergey Grigorenko <svipsa@gmail.com>
2019-10-01 21:05:58 +0300

    Update settings

---
+++ drupal/web/sites/default/settings.pantheon.php
@@ -0,0 +1,14 @@
+<?php
+
+$config_directories['sync'] = '../config/sync';
+$databases['default']['default'] = array (
+  'database' => 'pantheon',
+  'username' => 'pantheon',
+  'password' => 'b973f8c5c2b64f3882f2320d0b684eef',
+  'prefix' => '',
+  'host' => 'dbserver.dev.51b73c1d-15ea-4605-9b35-139337cefb1f.drush.in',
+  'port' => '19922',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'i15TsZIlfgj9EZBehw0d65+ihVOY4A59GQd0ewZE2J4=';
