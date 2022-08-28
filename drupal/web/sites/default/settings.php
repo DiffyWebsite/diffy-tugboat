e42c8b898768bd9bc2ba95a241578e7ce1477c03
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2022-08-27 17:22:59 -0700

    Add config sync folder.

--- drupal/web/sites/default/settings.php
+++ drupal/web/sites/default/settings.php
@@ -775,6 +775,7 @@ $settings['entity_update_batch_size'] = 50;
  *      the site settings remain consistent.
  */
 include __DIR__ . "/settings.pantheon.php";
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
 
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
