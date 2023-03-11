ba8900eccd055d4515794f074bb5213c70bd9145
nina_jeq <nina.jechiu@indrivo.com>
2023-03-11 15:03:30 +0200

    Install and configure Superfish Menu Module

--- drupal/vendor/composer/autoload_real.php
+++ drupal/vendor/composer/autoload_real.php
@@ -25,42 +25,19 @@ class ComposerAutoloaderInitd85c138d3fb33991d08e84b08a86d57a
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitd85c138d3fb33991d08e84b08a86d57a', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitd85c138d3fb33991d08e84b08a86d57a', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
         $includePaths[] = get_include_path();
         set_include_path(implode(PATH_SEPARATOR, $includePaths));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInitd85c138d3fb33991d08e84b08a86d57a::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInitd85c138d3fb33991d08e84b08a86d57a::getInitializer($loader));
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitd85c138d3fb33991d08e84b08a86d57a::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = \Composer\Autoload\ComposerStaticInitd85c138d3fb33991d08e84b08a86d57a::$files;
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequired85c138d3fb33991d08e84b08a86d57a($fileIdentifier, $file);
         }
@@ -69,11 +46,16 @@ class ComposerAutoloaderInitd85c138d3fb33991d08e84b08a86d57a
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequired85c138d3fb33991d08e84b08a86d57a($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
