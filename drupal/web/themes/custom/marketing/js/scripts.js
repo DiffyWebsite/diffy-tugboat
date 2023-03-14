5142d5b27de4534ee7c699345bd0ada4b8cc0abf
Dan G <nina.jechiu@indrivo.com>
2023-03-14 02:44:07 +0200

    Demo pop-up and fixes

--- drupal/web/themes/custom/marketing/js/scripts.js
+++ drupal/web/themes/custom/marketing/js/scripts.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
   $(function () {
     $('.navbar-toggler').click(function () {
       $('.navbar-block__mobile--overlay').toggleClass('js-navbar-block__mobile--show');
@@ -34,18 +34,56 @@
       var currentUrl = document.location.toString();
       if (currentUrl.indexOf('?') !== -1 && currentUrl.indexOf('utm_') !== -1) {
         var getParameters = currentUrl.substr(currentUrl.indexOf('?'));
-        $(context).find('a[href*="app.diffy.website"]').once('diffyUTMs').each(function(){
+        $(context).find('a[href*="app.diffy.website"]').once('diffyUTMs').each(function () {
           var href = $(this).attr('href');
           href = href + getParameters;
           $(this).attr('href', href);
         });
-        $(context).find('a[href^="/"]').once('diffyUTMs').each(function(){
+        $(context).find('a[href^="/"]').once('diffyUTMs').each(function () {
           var href = $(this).attr('href');
           href = href + getParameters;
           $(this).attr('href', href);
         });
       }
 
+    }
+  };
+
+})(jQuery, Drupal);
+
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.diffyDemo = {
+    attach: function (context, settings) {
+
+      // Gets the video src from the data-src on each button
+
+      var $videoSrc;
+      $('.video-btn').click(function () {
+        $videoSrc = $(this).data("src");
+      });
+      console.log($videoSrc);
+
+
+
+      // when the modal is opened autoplay it
+      $('#myModal').on('shown.bs.modal', function (e) {
+
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+
+      })
+
+
+
+      // stop playing the youtube video when I close the modal
+      $('#myModal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        $("#video").attr('src', $videoSrc);
+      })
     }
   };
 
